0a55293b142d2cba8dc87b27c306756794b392bc
Marat Zimnurov <zimtir@mail.ru>
2020-07-12 22:37:40 +0300

    Fix prefetch issues (#15)

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,22 +1,3 @@
-<script context="module">
-  export async function preload({ params, query }) {
-    // the `slug` parameter is available because
-    // this file is called [slug].svelte
-    const res = await this.fetch(`blog/${params.slug}.json`);
-    const data = await res.json();
-
-    if (res.status === 200) {
-      return { post: data };
-    } else {
-      this.error(res.status, data.message);
-    }
-  }
-</script>
-
-<script>
-  export let post;
-</script>
-
 <style>
   /*
 		By default, CSS is locally scoped to the component,
@@ -52,6 +33,27 @@
     margin: 0 0 0.5em 0;
   }
 </style>
+
+<script context="module">
+  import 'cross-fetch/polyfill'
+
+  export const preload = async ({ params, query }) => {
+    // the `slug` parameter is available because
+    // this file is called [slug].svelte
+    const res = await fetch(`http://localhost:3000/blog/${params.slug}.json`)
+    const data = await res.json()
+
+    if (res.status === 200) {
+      return { post: data }
+    } else {
+      console.log(res.status, data.message)
+    }
+  }
+</script>
+
+<script>
+  export let post
+</script>
 
 <svelte:head>
   <title>{post.title}</title>
