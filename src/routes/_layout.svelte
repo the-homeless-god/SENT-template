e91510a28719f4f04f7689b01bfabbed3cc1ed7b
Zimtir <zimtir@mail.ru>
2020-08-01 21:20:02 +0300

    refactor: use a prettier for a code

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,21 +1,12 @@
 <script>
   import Nav from '../components/Nav.svelte'
 
+  import 'layout.scss'
+
   export let segment
 </script>
 
-<style>
-  main {
-    position: relative;
-    max-width: 56em;
-    background-color: white;
-    padding: 2em;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-</style>
-
-<Nav {segment} />
+<Nav segment="segment" />
 
 <main>
   <slot />
